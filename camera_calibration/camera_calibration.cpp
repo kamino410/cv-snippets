8269e98d3946fcacaddbfbabc3c44b4795f91165
kamino410 <kamino.dev@gmail.com>
2018-09-29 07:35:40 +0900

    modify var names

--- camera_calibration/camera_calibration.cpp
+++ camera_calibration/camera_calibration.cpp
@@ -2,32 +2,30 @@
 
 /// Mono Calibration
 /// ------------------
-MonoCalibration::MonoCalibration(int horiBlockCount, int vertBlockCount,
-                                 float blockSize) {
-  horiCorner = horiBlockCount;
-  vertCorner = vertBlockCount;
+MonoCalibration::MonoCalibration(int hori_cnt, int vert_corner_cnt,
+                                 float block_size) {
+  hori = hori_cnt;
+  vert = vert_corner_cnt;
 
-  for (int j = 0; j < horiCorner * vertCorner; j++) {
-    cornersRel.push_back(cv::Point3f(blockSize * (j % horiCorner),
-                                     blockSize * (j / horiCorner), 0.0f));
+  for (int j = 0; j < hori * vert; j++) {
+    corners_local.push_back(
+        cv::Point3f(block_size * (j % hori), block_size * (j / hori), 0.0f));
   }
 }
 
 bool MonoCalibration::check_corners(cv::Mat img) {
-  imageSize = cv::Size(img.cols, img.rows);
+  image_size = cv::Size(img.cols, img.rows);
 
   std::vector<cv::Point2f> corners;
-  bool found =
-      cv::findChessboardCorners(img, cv::Size(horiCorner, vertCorner), corners);
+  bool found = cv::findChessboardCorners(img, cv::Size(hori, vert), corners);
   if (found) {
-    img.copyTo(imgVisualizeCorner);
+    img.copyTo(img_viz);
 
-    cornersIn3D.push_back(cornersRel);
-    cornersInImgs.push_back(corners);
+    corners_3d.push_back(corners_local);
+    corners_imgs.push_back(corners);
 
-    cv::drawChessboardCorners(imgVisualizeCorner,
-                              cv::Size(horiCorner, vertCorner),
-                              cv::Mat(corners), true);
+    cv::drawChessboardCorners(img_viz, cv::Size(hori, vert), cv::Mat(corners),
+                              true);
   }
 
   return found;
@@ -35,55 +33,52 @@ bool MonoCalibration::check_corners(cv::Mat img) {
 
 void MonoCalibration::calibrate() {
   std::vector<cv::Mat> rvecs, tvecs;
-  intrinsic = cv::Mat(3, 3, CV_64F).clone();
+  intr = cv::Mat(3, 3, CV_64F).clone();
   dist = cv::Mat(8, 1, CV_64F).clone();
-  cv::calibrateCamera(cornersIn3D, cornersInImgs, imageSize, intrinsic, dist,
-                      rvecs, tvecs);
+  cv::calibrateCamera(corners_3d, corners_imgs, image_size, intr, dist, rvecs,
+                      tvecs);
 }
 
 void MonoCalibration::save_params(std::string filename) {
   cv::FileStorage fs(filename.c_str(), cv::FileStorage::WRITE);
-  fs << "intrinsic" << intrinsic;
+  fs << "intr" << intr;
   fs << "dist" << dist;
   fs.release();
 }
 
 // Stereo Calibration
 // --------------------
-StereoCalibration::StereoCalibration(int horiBlockCount, int vertBlockCount,
-                                     float blockSize) {
-  horiCorner = horiBlockCount;
-  vertCorner = vertBlockCount;
+StereoCalibration::StereoCalibration(int hori_cnt, int vert_corner_cnt,
+                                     float block_size) {
+  hori = hori_cnt;
+  vert = vert_corner_cnt;
 
-  for (int j = 0; j < horiCorner * vertCorner; j++) {
-    cornersRel.push_back(cv::Point3f(blockSize * (j % horiCorner),
-                                     blockSize * (j / horiCorner), 0.0f));
+  for (int j = 0; j < hori * vert; j++) {
+    corners_local.push_back(
+        cv::Point3f(block_size * (j % hori), block_size * (j / hori), 0.0f));
   }
 }
 
 bool StereoCalibration::check_corners(cv::Mat img1, cv::Mat img2) {
-  imageSize = img1.size();
+  image_size = img1.size();
 
   std::vector<cv::Point2f> corners1;
   std::vector<cv::Point2f> corners2;
-  bool found = cv::findChessboardCorners(img1, cv::Size(horiCorner, vertCorner),
-                                         corners1) &&
-               cv::findChessboardCorners(img2, cv::Size(horiCorner, vertCorner),
-                                         corners2);
+  bool found =
+      cv::findChessboardCorners(img1, cv::Size(hori, vert), corners1) &&
+      cv::findChessboardCorners(img2, cv::Size(hori, vert), corners2);
   if (found) {
-    img1.copyTo(imgVisualizeCorner1);
-    img2.copyTo(imgVisualizeCorner2);
+    img1.copyTo(img_viz1);
+    img2.copyTo(img_viz2);
 
-    cornersIn3D.push_back(cornersRel);
-    cornersInImgs1.push_back(corners1);
-    cornersInImgs2.push_back(corners2);
+    corners_3d.push_back(corners_local);
+    corners_imgs1.push_back(corners1);
+    corners_imgs2.push_back(corners2);
 
-    cv::drawChessboardCorners(imgVisualizeCorner1,
-                              cv::Size(horiCorner, vertCorner),
-                              cv::Mat(corners1), true);
-    cv::drawChessboardCorners(imgVisualizeCorner2,
-                              cv::Size(horiCorner, vertCorner),
-                              cv::Mat(corners2), true);
+    cv::drawChessboardCorners(img_viz1, cv::Size(hori, vert), cv::Mat(corners1),
+                              true);
+    cv::drawChessboardCorners(img_viz2, cv::Size(hori, vert), cv::Mat(corners2),
+                              true);
   }
 
   return found;
@@ -91,28 +86,28 @@ bool StereoCalibration::check_corners(cv::Mat img1, cv::Mat img2) {
 
 void StereoCalibration::calibrate() {
   std::vector<cv::Mat> rvecs1, tvecs1;
-  intrinsic1 = cv::Mat(3, 3, CV_64F).clone();
+  intr1 = cv::Mat(3, 3, CV_64F).clone();
   dist1 = cv::Mat(5, 1, CV_64F).clone();
-  cv::calibrateCamera(cornersIn3D, cornersInImgs1, imageSize, intrinsic1, dist1,
+  cv::calibrateCamera(corners_3d, corners_imgs1, image_size, intr1, dist1,
                       rvecs1, tvecs1);
 
   std::vector<cv::Mat> rvecs2, tvecs2;
-  intrinsic2 = cv::Mat(3, 3, CV_64F).clone();
+  intr2 = cv::Mat(3, 3, CV_64F).clone();
   dist2 = cv::Mat(5, 1, CV_64F).clone();
-  cv::calibrateCamera(cornersIn3D, cornersInImgs2, imageSize, intrinsic2, dist2,
+  cv::calibrateCamera(corners_3d, corners_imgs2, image_size, intr2, dist2,
                       rvecs2, tvecs2);
 
   cv::Mat essential, fundamental;
-  cv::stereoCalibrate(cornersIn3D, cornersInImgs1, cornersInImgs2, intrinsic1,
-                      dist1, intrinsic2, dist2, imageSize, rot, tvec, essential,
+  cv::stereoCalibrate(corners_3d, corners_imgs1, corners_imgs2, intr1, dist1,
+                      intr2, dist2, image_size, rot, tvec, essential,
                       fundamental);
 }
 
 void StereoCalibration::save_params(std::string filename) {
   cv::FileStorage fs(filename.c_str(), cv::FileStorage::WRITE);
-  fs << "intrinsic1" << intrinsic1;
+  fs << "intr1" << intr1;
   fs << "dist1" << dist1;
-  fs << "intrinsic2" << intrinsic2;
+  fs << "intr2" << intr2;
   fs << "dist2" << dist2;
   fs << "rot" << rot;
   fs << "tvec" << tvec;
--- camera_calibration/camera_calibration.hpp
+++ camera_calibration/camera_calibration.hpp
@@ -3,21 +3,20 @@
 #include <opencv2/opencv.hpp>
 
 class MonoCalibration {
-  int horiCorner;
-  int vertCorner;
-  cv::Size imageSize;
-  std::vector<cv::Point3f> cornersRel;
-  std::vector<std::vector<cv::Point3f>> cornersIn3D;
-  std::vector<std::vector<cv::Point2f>> cornersInImgs;
+  int hori, vert;
+  cv::Size image_size;
+  std::vector<cv::Point3f> corners_local;
+  std::vector<std::vector<cv::Point3f>> corners_3d;
+  std::vector<std::vector<cv::Point2f>> corners_imgs;
 
   MonoCalibration() {}
 
  public:
-  cv::Mat imgVisualizeCorner;
-  cv::Mat intrinsic;
+  cv::Mat img_viz;
+  cv::Mat intr;
   cv::Mat dist;
 
-  MonoCalibration(int horiCrossCount, int vertCrossCount, float blockSize);
+  MonoCalibration(int, int, float);
 
   bool check_corners(cv::Mat img);
   void calibrate();
@@ -25,28 +24,26 @@ class MonoCalibration {
 };
 
 class StereoCalibration {
-  int horiCorner;
-  int vertCorner;
-
-  cv::Size imageSize;
-  std::vector<cv::Point3f> cornersRel;
-  std::vector<std::vector<cv::Point3f>> cornersIn3D;
-  std::vector<std::vector<cv::Point2f>> cornersInImgs1;
-  std::vector<std::vector<cv::Point2f>> cornersInImgs2;
+  int hori, vert;
+  cv::Size image_size;
+  std::vector<cv::Point3f> corners_local;
+  std::vector<std::vector<cv::Point3f>> corners_3d;
+  std::vector<std::vector<cv::Point2f>> corners_imgs1;
+  std::vector<std::vector<cv::Point2f>> corners_imgs2;
 
   StereoCalibration() {}
 
  public:
-  cv::Mat imgVisualizeCorner1;
-  cv::Mat imgVisualizeCorner2;
-  cv::Mat intrinsic1;
-  cv::Mat intrinsic2;
+  cv::Mat img_viz1;
+  cv::Mat img_viz2;
+  cv::Mat intr1;
+  cv::Mat intr2;
   cv::Mat dist1;
   cv::Mat dist2;
   cv::Mat rot;
   cv::Mat tvec;
 
-  StereoCalibration(int horiCrossCount, int vertCrossCount, float blockSize);
+  StereoCalibration(int, int, float);
 
   bool check_corners(cv::Mat img1, cv::Mat img2);
   void calibrate();
