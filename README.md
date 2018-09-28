0f1acd815a9cf7d523520bf2707ee0217b5960e2
kamino410 <kamino.dev@gmail.com>
2018-09-28 20:13:00 +0900

    add ximea/main.cpp

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ Simple sample code to take a picture using Canon EDSDK.
 ## graycode
 Obtain projector-camera relationship using gray code pattern.
 [https://kamino.hatenablog.com/entry/opencv-graycode](https://kamino.hatenablog.com/entry/opencv-graycode)
+
+## ximea
+Simple sample code to get image from ximea camera.
---
+++ ximea/main.cpp
@@ -0,0 +1,52 @@
+#include <opencv2/opencv.hpp>
+#include <xiApi.h>
+
+#define HandleResult(res, place)                 \
+  if (res != XI_OK) {                            \
+    printf("Error after %s (%d)\n", place, res); \
+    exit(1);                                     \
+  }
+
+int main() {
+  HANDLE xiH = NULL;
+  XI_RETURN stat = XI_OK;
+
+  // Allocate memory for ximea image
+  XI_IMG img;
+  memset(&img, 0, sizeof(img));
+  img.size = sizeof(XI_IMG);
+
+  // Connect to camera
+  stat = xiOpenDevice(0, &xiH);
+  HandleResult(stat, "xiOpenDevice");
+
+  // Configuration
+  stat = xiSetParamInt(xiH, XI_PRM_EXPOSURE, 100000);
+  HandleResult(stat, "xiGetParam (exposure)");
+  stat = xiSetParamFloat(xiH, XI_PRM_GAIN, 1);
+  HandleResult(stat, "xiGetParam (gain)");
+  stat = xiSetParamFloat(xiH, XI_PRM_GAMMAY, 1.0);
+  HandleResult(stat, "xiGetParam (gamma y)");
+
+  // Start acquisition
+  stat = xiStartAcquisition(xiH);
+  HandleResult(stat, "xiStartAcquisition");
+
+  // Preview the output from camera
+  do {
+    // Get image from camera
+    stat = xiGetImage(xiH, 5000, &img);
+    HandleResult(stat, "xiGetImage");
+
+    // Convert XI_IMG into cv::Mat
+    cv::Mat cvimg(cv::Size((int)img.width, (int)img.height), CV_8UC1);
+    cvimg.data = (unsigned char*)img.bp;
+
+    // Show
+    cv::imshow("test", cvimg);
+  } while (cv::waitKey(1) == -1);
+
+  // Terminate
+  xiStopAcquisition(xiH);
+  xiCloseDevice(xiH);
+}
